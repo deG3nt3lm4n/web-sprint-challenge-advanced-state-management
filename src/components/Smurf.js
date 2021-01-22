601c552f3d50156bad71433d23723c00849799dd
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-01-22 15:33:18 -0500

    Create a CodeGrade submission

--- src/components/Smurf.js
+++ src/components/Smurf.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Smurf extends React.Component {
     render() {
         const { smurf } = this.props;
-        console.log(smurf)
+
 
         return(
             <div data-testid="smurf" className="card">
